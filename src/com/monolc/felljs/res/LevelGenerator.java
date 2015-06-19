bd4ceeaad93c70b2d402b0b40f081f4ce3fc5581
nulltitleexception <clbprfftt@gmail.com>
2015-06-19 19:09:42 -0400

    lua

--- src/com/monolc/felljs/res/LevelGenerator.java
+++ src/com/monolc/felljs/res/LevelGenerator.java
@@ -10,7 +10,7 @@ public class LevelGenerator {
 	LuaValue	terrain;
 	public LevelGenerator(String name) {
 		Globals globals = JsePlatform.standardGlobals();
-		terrain = globals.loadfile("res/terrain/" + name + ".lua");
+		terrain = globals.load(globals.loadfile("res/terrain/" + name + ".lua").tojstring().replace("!=", "~="));
 	}
 	public Level createDungeonLevel(int width, int height) {
 		return new Level(width, height, terrain.call(LuaValue.valueOf(width), LuaValue.valueOf(height)).tojstring());
