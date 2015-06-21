6a7faef6c673c227e6317bcce317edf39bf737c4
nulltitleexception <clbprfftt@gmail.com>
2015-06-20 23:27:49 -0400

    dungeon test

--- res/terrain/dungeon.lua
+++ res/terrain/dungeon.lua
@@ -11,6 +11,13 @@ for a=0,xLen,1 do
 	end
 end
 
+local roomID = 1;
+
+function getRoomID()
+	roomID = roomID+1
+	return (roomID-1)
+end
+
 function placeRoom()
 	local x = (math.random(1,xLen/2) * 2) - 1
 	local y = (math.random(1,yLen/2) * 2) - 1
@@ -23,9 +30,10 @@ function placeRoom()
 			end
 		end
 	end
+	local rid = getRoomID()
 	for a=x,x+w,1 do
 		for b=y,y+h,1 do
-			cols[a][b] = x + (y * xLen);
+			cols[a][b] = rid;
 		end
 	end
 end
@@ -48,26 +56,27 @@ function canBranch(x, y, dir)
 	end
 end
 
-function branch(x, y)
+function branch(x, y, id)
+	id = id or getRoomID()
 	while(canBranch(x,y,1) or canBranch(x,y,2) or canBranch(x,y,3) or canBranch(x,y,4)) do
 		local dir = math.random(1,4)
 		if (canBranch(x,y,dir)) then
 			if (dir == 1) then
-				cols[x][y+1] = 1
-				cols[x][y+2] = 1
-				branch(x, y+2)
+				cols[x][y+1] = id
+				cols[x][y+2] = id
+				branch(x, y+2, id)
 			elseif (dir == 2) then
-				cols[x+1][y] = 1
-				cols[x+2][y] = 1
-				branch(x+2, y)
+				cols[x+1][y] = id
+				cols[x+2][y] = id
+				branch(x+2, y, id)
 			elseif (dir == 3) then
-				cols[x][y-1] = 1
-				cols[x][y-2] = 1
-				branch(x, y-2)
+				cols[x][y-1] = id
+				cols[x][y-2] = id
+				branch(x, y-2, id)
 			elseif (dir == 4) then
-				cols[x-1][y] = 1
-				cols[x-2][y] = 1
-				branch(x-2, y)
+				cols[x-1][y] = id
+				cols[x-2][y] = id
+				branch(x-2, y, id)
 			else
 				print "Lua Error: perfectmaze.lua->branch()"
 			end
@@ -76,6 +85,81 @@ function branch(x, y)
 end
 
 branch((math.random((xLen-1)/2)*2)+1, (math.random((yLen-1)/2)*2)+1)
+
+for a=1,xLen,2 do
+	for b=1,yLen,2 do
+		if (cols[a][b] == -1) then
+			branch(a, b)
+		end
+	end
+end
+
+function isAdj(id, x, y)
+	return ((x > 0 and cols[x-1][y] == id) or (y > 0 and cols[x][y - 1] == id) or (x < xLen and cols[x+1][y] == id) or (y < xLen and cols[x][y+1] == id))
+end
+
+function merge(id1, id2)
+	local conx = {}
+	local cony = {}
+	local cond = {}
+	local conNum = 0
+	for a=0,xLen,1 do
+		for b=0,yLen,1 do
+			if (isAdj(id1,a,b) and isAdj(id2,a,b)) then
+				conx[conNum] = a
+				cony[conNum] = b
+				cond[conNum] = false;
+				conNum = conNum + 1
+			end
+		end
+	end
+	for i=0,math.min(conNum-1,math.random(0, math.max(2,(conNum*0.05) + 2))),1 do
+		local n = math.random(0,conNum-1)
+		while (cond[n]) do
+			n = math.random(0,conNum-1)
+		end
+		cols[conx[n]][cony[n]] = id1
+	end
+end
+
+function floodFillPos(x, y, id)
+	if (cols[x][y] >= 0) then
+		cols[x][y] = id
+		floodFillPos(x+1, y, id)
+		floodFillPos(x-1, y, id)
+		floodFillPos(x, y+1, id)
+		floodFillPos(x, y-1, id)
+	end
+end
+
+function floodFill(id)
+id = id or 1
+	for a=0,xLen,1 do
+		for b=0,yLen,1 do
+			if (cols[a][b] == id) then
+				floodFillPos(a,b,id)
+			end
+		end
+	end
+end
+
+function getID(min, max)
+	for a=0,xLen,1 do
+		for b=0,yLen,1 do
+			if ((cols[a][b] >= min) and (!max or (cols[a][b] <= max))) then
+				return cols[a][b]
+			end
+		end
+	end
+	return min-1
+end
+
+local nextID = getID(2)
+while (nextID >= 2) do
+	merge(1,nextID)
+	floodFill()
+	nextID = getID(2)
+end
 
 for a=0,xLen,1 do
 	for b=0,yLen,1 do
